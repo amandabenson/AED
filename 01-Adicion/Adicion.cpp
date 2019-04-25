27a805d4f61d3939336a39d67cc2762e91f4766f
Vane Benson <32781747+vanesabenson@users.noreply.github.com>
2019-04-25 03:03:11 -0300

    agregar adicion.cpp

---
+++ 01-Adicion/Adicion.cpp
@@ -0,0 +1,14 @@
+/*
+Trabajo #1 – Resolución de Problemas — Adición
+Amanda Vanesa Benson
+24/04/2019
+*/
+
+#include <iostream>
+
+int main(){
+int a,b;
+std::cin >> a;
+std::cin >> b;
+std::cout << a+b;
+}
